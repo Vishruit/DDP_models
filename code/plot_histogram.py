096c4be0f00ac2b46eb0ba38b93255147ef89ccc
Vishruit <vishruit@gmail.com>
2017-04-23 03:41:57 +0530

    Added code for plot methods histograms

--- code/plot_histogram.py
+++ code/plot_histogram.py
@@ -7,44 +7,80 @@ import h5py, numpy as np
 # train_file_name,  dataset_keyword = '../data_small_100.h5', 'data_small'
 train_file_name,  dataset_keyword = '/home/prabakaran/Vishruit/DDP/DATA_small_hdf/data_small_100.h5', 'data_small'
 
-train_set_data = HDF5Matrix(train_file_name, dataset_keyword)
 
-hist, bin_edges = np.histogram(train_set_data[0:202], normed =False, bins=100)
+def plot_histos():
+    global train_file_name,  dataset_keyword
+    train_set_data = HDF5Matrix(train_file_name, dataset_keyword)
 
-print(hist, bin_edges)
-# plt.hist(train_set_data[0:202].reshape((202*100*256*320,1)), bins='auto')  # plt.hist passes it's arguments to np.histogram
-plt.hist(hist, bins=bin_edges)  # plt.hist passes it's arguments to np.histogram
-plt.title("Histogram with 'auto' bins")
-plt.show()
+    hist, bin_edges = np.histogram(train_set_data[0:202], normed =False, bins=100)
+
+    print(hist, bin_edges)
+    # plt.hist(train_set_data[0:202].reshape((202*100*256*320,1)), bins='auto')  # plt.hist passes it's arguments to np.histogram
+    plt.hist(hist, bins=bin_edges)  # plt.hist passes it's arguments to np.histogram
+    plt.title("Histogram with 'auto' bins")
+    plt.show()
 
 
-from skimage import data
-from skimage.filters import threshold_otsu, threshold_local
+def demo1():
+    # Otsu's demo
+    from skimage import data
+    from skimage.filters import threshold_otsu, threshold_local
+    
+    image = data.page()
 
+    global_thresh = threshold_otsu(image)
+    binary_global = image > global_thresh
 
-image = data.page()
+    block_size = 35
+    adaptive_thresh = threshold_local(image, block_size, offset=10)
+    binary_adaptive = image > adaptive_thresh
 
-global_thresh = threshold_otsu(image)
-binary_global = image > global_thresh
+    fig, axes = plt.subplots(nrows=3, figsize=(7, 8))
+    ax = axes.ravel()
+    plt.gray()
 
-block_size = 35
-adaptive_thresh = threshold_local(image, block_size, offset=10)
-binary_adaptive = image > adaptive_thresh
+    ax[0].imshow(image)
+    ax[0].set_title('Original')
 
-fig, axes = plt.subplots(nrows=3, figsize=(7, 8))
-ax = axes.ravel()
-plt.gray()
+    ax[1].imshow(binary_global)
+    ax[1].set_title('Global thresholding')
 
-ax[0].imshow(image)
-ax[0].set_title('Original')
+    ax[2].imshow(binary_adaptive)
+    ax[2].set_title('Adaptive thresholding')
 
-ax[1].imshow(binary_global)
-ax[1].set_title('Global thresholding')
+    for a in ax:
+        a.axis('off')
 
-ax[2].imshow(binary_adaptive)
-ax[2].set_title('Adaptive thresholding')
+    plt.show()
 
-for a in ax:
-    a.axis('off')
+def demo2():
+    # Another demo
+    import matplotlib.pyplot as plt
+    from skimage import data
+    try:
+        from skimage import filters
+    except ImportError:
+        from skimage import filter as filters
+    from skimage import exposure
 
-plt.show()
+    camera = data.camera()
+    val = filters.threshold_otsu(camera)
+
+    hist, bins_center = exposure.histogram(camera)
+
+    plt.figure(figsize=(9, 4))
+    plt.subplot(131)
+    plt.imshow(camera, cmap='gray', interpolation='nearest')
+    plt.axis('off')
+    plt.subplot(132)
+    plt.imshow(camera < val, cmap='gray', interpolation='nearest')
+    plt.axis('off')
+    plt.subplot(133)
+    plt.plot(bins_center, hist, lw=2)
+    plt.axvline(val, color='k', ls='--')
+
+    plt.tight_layout()
+    plt.show()
+
+demo1()
+demo2()
