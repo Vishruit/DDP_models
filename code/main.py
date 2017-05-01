77e62fb38a7adc86a87c2e773ffe027f4897e1d1
vishruit <vishruit@gmail.com>
2017-05-01 16:25:49 +0530

    Added allow_growth = True

--- code/main.py
+++ code/main.py
@@ -286,7 +286,11 @@ if __name__ == "__main__":
     import keras.backend.tensorflow_backend as K
 
     with K.tf.device('/gpu:0'):
-        K.set_session(K.tf.Session(config=K.tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)))
+        # gpu_options.allow_growth = True
+        # config = tf.ConfigProto()
+        config = K.tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
+        config.gpu_options.allow_growth = True
+        K.set_session(K.tf.Session(config=config))
         model = define_model(model_initializer, lr, verbose,restart=restart)
 
     img_size = 32
