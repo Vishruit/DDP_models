53ebeb9fa3fc3cc6a6afaa56023f49f84d82e8df
Vishruit <vishruit@gmail.com>
2017-05-06 02:56:19 +0530

    Plot via Canvaswq

--- code/main.py
+++ code/main.py
@@ -2,6 +2,10 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+# Reference counted: Helps with segmentation fault
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 import keras
 from keras import initializers
 import keras.backend as K
@@ -52,6 +56,40 @@ class TestCallback(Callback):
 
 
 def plot_video_plot(epoch,x_test):
+    global batch_size, visualization_filepath
+    video_index = [1,5,10,15,20,25,30]
+    frame_index = [1,5,10,25,40,50,60,75,90,99]
+    decoded_imgs = model.predict(x_test[video_index], batch_size=batch_size)
+
+    fig = Figure()
+    canvas = FigureCanvas(fig)
+
+    fig, axi = plt.subplots(2, len(frame_index), figsize=(20,4))
+    # ax = plt.subplot(2, len(frame_index), 1)
+    for (video, vid_it) in zip(video_index, range(len(video_index))):
+        # plt.figure(figsize=(20, 4))
+        for i in range(len(frame_index)):
+            print(video, vid_it, i, len(frame_index))
+            # ax = plt.subplot(2, len(frame_index), i + 1)
+            ax1 = fig.add_subplot(2,len(frame_index),i+1)
+            ax2 = fig.add_subplot(2,len(frame_index),i+len(frame_index)+1)
+            # ax1,ax2 = axi[:,i]
+            ax1.imshow(x_test[video].reshape(frames, 256, 320)[frame_index[i],...])
+            plt.gray()
+            ax1.get_xaxis().set_visible(False)
+            ax1.get_yaxis().set_visible(False)
+            print ('this passes the test')
+            #plt.imshow(decoded_imgs[vid_it].reshape(frames, 256, 320)[frame_index[i],...])
+            plt.gray()
+            ax2.get_xaxis().set_visible(False)
+            ax2.get_yaxis().set_visible(False)
+        canvas.print_figure( visualization_filepath+ 'reconstruction_vid_'+str(video)+'_Epoch_'+str(epoch)+'.png' )
+        # plt.savefig( visualization_filepath+ 'reconstruction_vid_'+str(video)+'_Epoch_'+str(epoch)+'.png' )
+    return
+
+
+
+def plot_video_plot2(epoch,x_test):
     global batch_size, visualization_filepath
     video_index = [1,5,10,15,20,25,30]
     frame_index = [1,5,10,25,40,50,60,75,90,99]
