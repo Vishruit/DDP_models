2e72d8522fa69ce7408fb6db0d8a6fc8215b9a4a
Vishruit <vishruit@gmail.com>
2017-05-10 18:32:32 +0530

    Minor fix

--- code/constants.py
+++ code/constants.py
@@ -8,13 +8,15 @@ append_CSVfile_FLAG = False
 #data = (nsamples, 202*100*256*320) float32
 
 experiment_num = '6'
+
 experiment_root = './exp'+experiment_num+'/'
 visualization_filepath = './exp'+experiment_num+'/visualizations/'
 visualization_filepath_test_time = './exp'+experiment_num+'/visualizations/Test_time/'
+code_base_file_path = './exp' + experiment_num +'/code/'
+
 filepath_best_weights='./exp'+experiment_num+'/save_dir/weights.best.hdf5'
 filepath_chpkt_weights = './exp'+experiment_num+'/save_dir/CheckPoint/'
 filepath_csvLogger = './exp'+experiment_num+'/save_dir/CheckPoint/csv_log_file.csv'
-code_base_file_path = './exp' + experiment_num +'/code/'
 
 img_size = 32
 num_channels = 3
