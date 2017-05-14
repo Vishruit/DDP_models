ddd8a10fc58d07386b375521934a3902047c044a
Vishruit <vishruit@gmail.com>
2017-05-14 20:10:42 +0530

    Minor rename

--- code/network.py
+++ code/network.py
@@ -48,7 +48,7 @@ def define_model(init,lr,verbose,restart):
     # x = Conv3D(128, (3, 3, 3), activation='relu', padding='same', kernel_initializer=init)(x)
     # x = UpSampling3D((2, 2, 2))(x)
 
-    decoded = Conv3D(1, (3, 3, 3), activation='sigmoid', padding='same', name='conv3d_08')(x)
+    decoded = Conv3D(1, (3, 3, 3), activation='sigmoid', padding='same', name='conv3d_09')(x)
     decoded = Reshape((frames, height, width), name='decoded')(decoded)
 
     # x = Dropout(dropout_rate)(x)
